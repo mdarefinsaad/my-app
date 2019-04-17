32e07d764aa4a649370d328ea14e6fc2f59c3cc7
unknown <mdarefinsaad@gmail.com>
2019-04-17 18:16:02 +0600

    create an ingredient model

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,15 @@
+export class Ingredient {
+
+  // Way 1
+  // public name:string;
+  // public amount: number;
+  //
+  // constructor(name: string, amount: number){
+  //   this.name = name;
+  //   this.amount = amount;
+  // }
+
+
+  // Way2
+  constructor(public name: string, public amount: number) {}
+}
